61c60cd4ccda6e25b6dd07401cabee7aa452d80b
Aparna Sharma <sharma.apa@husky.neu.edu>
2018-06-06 03:00:08 -0400

    Assignment widget

---
+++ components/AssignmentWidget.js
@@ -0,0 +1,134 @@
+import React, {Component} from 'react'
+import {View, TextInput, ScrollView, Alert,Keyboard} from 'react-native'
+import {FormLabel, FormInput, FormValidationMessage, Button, Text, Card} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+class AssignmentWidget extends Component {
+    static navigationOptions = {title: 'AssignmentWidget'}
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: '',
+            description: '',
+            points: '',
+            widgetId: '',
+            topicId: ''
+
+        }
+
+        this.updateAssignment= this.updateAssignment.bind(this);
+    }
+
+    updateForm(newState) {
+        this.setState(newState)
+    }
+
+    updateAssignment() {
+
+
+         fetch(("https://cs5610-react-native-aparna.herokuapp.com/api/assignment/" + this.state.widgetId),
+            {
+                body: JSON.stringify({
+                    'id':this.state.widgetId,
+                    'title': this.state.title,
+                    'description': this.state.description,
+                    'points': this.state.points
+                }),
+                headers: {'Content-Type': 'application/json'},
+                method: 'PUT'
+            })
+            .then(this.findAssignmentById(this.state.widgetId))
+             .then(Alert.alert("Changes Saved"))
+
+    }
+
+
+    componentDidMount() {
+        const {navigation} = this.props;
+        const widgetId = navigation.getParam("widgetId");
+        const topicId = navigation.getParam("topicId");
+        this.setState({
+            topicId: topicId,
+            widgetId: widgetId
+        })
+
+        this.findAssignmentById(widgetId).then((assignment) => this.setState({
+            title: assignment.title,
+            points: assignment.points,
+            description: assignment.description
+
+        }))
+
+
+    }
+
+
+    findAssignmentById(widgetId) {
+        return fetch(("https://cs5610-react-native-aparna.herokuapp.com/api/assignment/AID").replace('AID', widgetId))
+            .then(response => (response.json()))
+
+    }
+
+    render() {
+
+        return (
+            <ScrollView>
+                <FormLabel>Title</FormLabel>
+                <FormInput value={this.state.title} onChangeText={
+                    text => this.updateForm({title: text})
+                }/>
+                <FormValidationMessage>
+                    Title is required
+                </FormValidationMessage>
+
+
+                <FormLabel>Points</FormLabel>
+                <FormInput
+                    value={String(this.state.points)}
+                    keyboardType='numeric'
+                    onChangeText={
+                        text => this.updateForm({points: text})
+                    }/>
+                <FormValidationMessage>
+                    points is required
+                </FormValidationMessage>
+
+
+                <FormLabel>Description</FormLabel>
+                <FormInput value={this.state.description} onChangeText={
+                    text => this.updateForm({description: text})
+                }/>
+                <FormValidationMessage>
+                    Description is required
+                </FormValidationMessage>
+
+
+                <Button backgroundColor="green" color="white" title="Save"
+                        onPress={() => this.updateAssignment(this.state.widgetId)}/>
+
+                <Button backgroundColor="red" color="white" title="Cancel" onPress={() => this.props.navigation
+                    .navigate("WidgetList", {topicId: this.state.topicId})}/>
+
+                <Text h3 style={{padding:15}}>Preview</Text>
+
+                <View style={{flexDirection: 'row', padding:15}}>
+                    <Text h4> {this.state.title} </Text><Text h4> {this.state.points}pts</Text>
+                </View>
+
+                <Text>{this.state.description}</Text>
+
+                <View>
+                <Text h4>Essay Answer</Text>
+                <TextInput editable={false} multiline={true} numberOfLines={3}/>
+                </View>
+                <Text h4>Upload File</Text>
+                <FormInput style={{border: 2}}/>
+
+                <Text h4> Submit a link</Text>
+            </ScrollView>
+        )
+    }
+}
+
+            export default AssignmentWidget
